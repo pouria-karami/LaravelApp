9337a472954e999b809e5a10d783c621f2cb607b
Pouria <pouria_karami@yahoo.com>
2022-05-26 19:29:49 +0430

    fix img.

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -8,7 +8,7 @@
             @foreach ($listings as $listing)
                 <div class="bg-gray-50 border border-gray-200 rounded p-6">
                     <div class="flex">
-                        <img class="hidden w-48 mr-6 md:block" src="public/images/no-image.png" alt=""/>
+                        <img class="hidden w-48 mr-6 md:block" src="{{asset('images/no-image.png')}}" alt=""/>
                         <div>
                             <h3 class="text-2xl">
                                 <a href="/Listings/{{$listing['id']}}">{{$listing->title}}</a>
